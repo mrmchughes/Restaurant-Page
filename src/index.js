870126271440121c5a9474a35128c5c7975be8f5
mrmchughes <mrmchughes27@gmail.com>
2021-09-13 14:18:44 -0700

    codepen commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { mainPage } from "./mainPage.js";
+
+window.onload = function(){
+    mainPage();
+}
---
+++ src/mainPage.js
@@ -0,0 +1,10 @@
+export function mainPage(){
+    console.log('I am mainPage, and I am working!')
+
+    const headline = document.createElement('h1');
+    headline.innerHTML = "Boston's Best Bakery";
+
+    const mainPageCopy = document.createElement('p');
+    mainPageCopy.innerText = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
+
+}
